15c145d9ede4f0eb6bb5412c8f39ea822bd8ca45
Ani12234 <anirudhkulkarni733@gmail.com>
2025-02-21 12:00:21 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -37,7 +37,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(
   cors({
-    origin: "https://suits-three.vercel.app/",
+    origin: "https://suits-three.vercel.app",
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
