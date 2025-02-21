b4e129954109025285b7dafda8fb38347f13d862
Ani12234 <anirudhkulkarni733@gmail.com>
2025-02-21 11:08:43 +0530

    loader fixed

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "./Navbar";
 import Hero from "./Hero";
 import Features from "./Features";
-import Loader from "./Loader";
+import Loader from "./loader";
 
 const HomePage = () => {
   return (
